f752392d6c9c5c66cbadd6b0e00d27ad824100ad
Andrey Gabriel Ben <andreygabrielben@gmail.com>
2026-04-02 20:29:17 -0300

    Criar um método que verifica se o valor para uso é positivo

--- NewBank.java
+++ NewBank.java
@@ -1,41 +1,55 @@
+
 class Conta {
+
     String titular;
     double saldo;
 
-    public Conta (String titular, double saldo) {
-        this.titular =  titular;
-        this.saldo =  saldo;
+    public Conta(String titular, double saldo) {
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public boolean  verificarValor(double valor){
+        if (valor > 0){
+            return true;
+        } else {
+            System.out.println("Valor inválido");
+            return false;
+        }
     }
 
 
-    public void depositar(double valor){
+    public void depositar(double valor) {
+        if(!verificarValor(valor)){return;}
         saldo += valor;
     }
 
-    public void sacar(double valor){
+    public void sacar(double valor) {
+        if(!verificarValor(valor)){return;}
         if (saldo < valor) {
             System.out.println("Saldo insuficiente, saque negado");
         } else {
             saldo -= valor;
+            System.out.println("Saldo realizado de: R$" + valor);
         }
     }
 
-    public String getTitular(){
+    public String getTitular() {
         return titular;
     }
 
-    public double getSaldo(){
+    public double getSaldo() {
         return saldo;
     }
 }
 
+public class NewBank {
 
-
-
-
-
-public class NewBank{
     public static void main(String[] args) {
-        
+
+
+
+
+
     }
 }
